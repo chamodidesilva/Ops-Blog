beafb24264675f6728e5b33b0ede54fc0e0225c7
Chamodi De Silva <chamodidesil@gmail.com>
2025-11-24 14:34:32 +0530

    feat(monitoring): monitor flask app request count

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,11 +1,23 @@
 import os
+import time
 
-from flask import Flask
+from flask import Flask, request, abort, Response
+from prometheus_client import Counter, Histogram, generate_latest, Gauge
+#from dotenv import load_dotenv
 
+#load_dotenv()
+
+# Prometheus metrics
+#endpoint_latency = Histogram('endpoint_latency_seconds', 'Endpoint response time', ['endpoint'])
+
+REQUEST_COUNT = Counter('hello_request_count', 'Number of hello endpoint calls')
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+
+    #app.config["PROMETHEUS_HEX"] = os.getenv('PROMETHEUS_HEX')
+
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
@@ -24,9 +36,36 @@ def create_app(test_config=None):
     except OSError:
         pass
 
+    # @app.before_request
+    # def start_timer():
+    #     request.start_time = time.time()
+
+    # @app.after_request
+    # def track_metrics(response):
+    #     if request.path != '/favicon.ico' and request.path != '/metrics':
+    #         # Track load time
+    #         latency = time.time() - request.start_time
+    #         endpoint_latency.labels(endpoint=request.path).observe(latency)
+
+    #     return response
+    
+    
     # a simple page that says hello
     @app.route('/hello')
     def hello():
+        REQUEST_COUNT.inc()
         return 'Hello, World!'
 
+    @app.route('/metrics')
+    def metrics():
+        # auth_header = request.headers.get("Authorization")
+        # if auth_header is None or not auth_header.startswith("Bearer "):
+        #     abort(401)  # Unauthorized if no bearer token is provided
+
+        # token = auth_header.split(" ")[1]
+        # if token != app.config["PROMETHEUS_HEX"]:
+        #     abort(403)  # Forbidden
+        return generate_latest(), 200, {'Content-Type': 'text/plain'}
+    
     return app
+
