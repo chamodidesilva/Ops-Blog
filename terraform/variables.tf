1c1bfdb01f345d2dde2eaba95348d52f0a9ee9d9
chamodidesilva <chamodidesil@gmail.com>
2026-05-04 14:39:01 +0530

    feat(aws): add Terraform variable declarations

---
+++ terraform/variables.tf
@@ -0,0 +1,30 @@
+variable "allowed_ips" {
+  type        = string
+  description = "Restrict ECS task access in security group only to my IP for testing"
+}
+
+variable "flask_secret_value" {
+  type        = string
+  description = "Value for the FLASK_SECRET"
+}
+
+variable "ecr_image_tag" {
+  type        = string
+  description = "Tag for the ECR image"
+}
+
+variable "region" {
+  type        = string
+  description = "AWS region"
+}
+
+variable "acc_id" {
+  type        = string
+  description = "AWS account ID"
+}
+
+variable "subnet_az" {
+  type        = string
+  description = "Availability zone for the subnet"
+}
+
