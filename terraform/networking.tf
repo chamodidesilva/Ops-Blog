9a84460c2aa5ac649e982336f9f11a49855864b2
chamodidesilva <chamodidesil@gmail.com>
2026-05-04 14:27:16 +0530

    feat(aws): add ECS task security group with DNS support for VPC

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -1,5 +1,7 @@
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_support   = true 
+  enable_dns_hostnames = true 
 
   tags = {
     Name = "main"
@@ -9,6 +11,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public_1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
+  availability_zone = var.subnet_az 
 
   tags = {
     Name = "public_1"
@@ -39,5 +42,29 @@ resource "aws_route_table" "rt" {
 resource "aws_route_table_association" "rt_assoc" {
   subnet_id      = aws_subnet.public_1.id
   route_table_id = aws_route_table.rt.id
+}
+
+resource "aws_security_group" "flask_sg" {
+  name        = "ops-blog-sg"
+  description = "Restrict access to ECS task"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Project = "Ops-Blog"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.flask_sg.id
+  cidr_ipv4         = "${var.allowed_ips}/32"
+  from_port         = 5000
+  ip_protocol       = "tcp"
+  to_port           = 5000
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.flask_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
 }
 
