7e3ccc4db1bd78439fe37aea681e9927f353bb46
chamodidesilva <chamodidesil@gmail.com>
2026-06-13 12:50:15 +0530

    feat(terraform): add ALB and related network components

--- terraform/containers.tf
+++ terraform/containers.tf
@@ -96,12 +96,14 @@ resource "aws_ecs_task_definition" "flask_app" {
 }
 
 resource "time_sleep" "wait_for_DNS" {
+  count           = var.environment_active ? 1 : 0
   depends_on = [aws_efs_mount_target.flask_efs_mount]
 
   create_duration = "75s"
 }
 
 resource "aws_ecs_service" "flask_service" {
+  count           = var.environment_active ? 1 : 0
   name            = "flask-service"
   cluster         = aws_ecs_cluster.flask_cluster.id
   task_definition = aws_ecs_task_definition.flask_app.arn
@@ -119,6 +121,12 @@ resource "aws_ecs_service" "flask_service" {
     subnets          = [aws_subnet.public_1.id]
     security_groups  = [aws_security_group.flask_sg.id]
     assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.flask_tg[0].arn
+    container_name   = "ops-blog-flask-app"
+    container_port   = 5000
   }
 }
 
